91b481d188aa3ea32f14c5844dc3d2e76615e625
Chandra Sekar S <chandru.in@gmail.com>
2012-04-01 23:17:13 +0530

    Avoid shared memory.

--- pubsub.go
+++ pubsub.go
@@ -19,81 +19,96 @@
 //
 // Topic names must be strings and messages of any type
 // can be published. A topic can have any number of subcribers and
-// all of them receive any message published to the topic.
+// all of them receive the messages published to the topic.
 package pubsub
-
-import "sync"
 
 type any interface{}
 
 // PubSub is a collection of topics.
 type PubSub struct {
-	topics map[string]map[chan any]bool
-	mx     *sync.RWMutex
+	topics                   map[string]map[chan any]bool
+	sub, subOnce, pub, unsub chan cmd
+}
+
+type cmd struct {
+	topic string
+	data  any
 }
 
 // New creates a new PubSub.
 func New() *PubSub {
 	topics := make(map[string]map[chan any]bool)
-	mx := new(sync.RWMutex)
-	return &PubSub{topics, mx}
+	ps := PubSub{topics, make(chan cmd), make(chan cmd), make(chan cmd), make(chan cmd)}
+	go ps.start()
+	return &ps
 }
 
 // Sub returns a channel on which messages published to
 // the specified topic can be received.
 func (ps *PubSub) Sub(topic string) chan any {
-	return ps.addSub(topic, false)
+	ch := make(chan any)
+	ps.sub <- cmd{topic, ch}
+	return ch
 }
 
 // SubOnce is similar to Sub, but only the first message
 // published to the topic can be received.
 func (ps *PubSub) SubOnce(topic string) chan any {
-	return ps.addSub(topic, true)
+	ch := make(chan any)
+	ps.subOnce <- cmd{topic, ch}
+	return ch
 }
 
 // Pub publishes the given message to all subscribers of
 // the specified topic.
 func (ps *PubSub) Pub(topic string, msg any) {
-	removes := make([]chan any, 0, 100)
+	ps.pub <- cmd{topic, msg}
+}
 
-	ps.mx.RLock()
-	for ch, once := range ps.topics[topic] {
-		ch <- msg
-		if once {
-			removes = append(removes, ch)
+// Unsub unsubscribes the given channel from the specified
+// topic.
+func (ps *PubSub) Unsub(topic string, ch chan any) {
+	ps.unsub <- cmd{topic, ch}
+}
+
+func (ps *PubSub) start() {
+	for {
+		select {
+		case cmd := <-ps.sub:
+			ps.add(cmd.topic, cmd.data.(chan any), false)
+
+		case cmd := <-ps.subOnce:
+			ps.add(cmd.topic, cmd.data.(chan any), true)
+
+		case cmd := <-ps.pub:
+			ps.send(cmd.topic, cmd.data.(any))
+
+		case cmd := <-ps.unsub:
+			ps.remove(cmd.topic, cmd.data.(chan any))
 		}
 	}
-	ps.mx.RUnlock()
-
-	if len(removes) > 0 {
-		ps.Unsub(topic, removes...)
-	}
 }
 
-// Unsub unsubscribed the given channels from the specified
-// topic.
-func (ps *PubSub) Unsub(topic string, chans ...chan any) {
-	ps.mx.Lock()
-	for _, ch := range chans {
-		delete(ps.topics[topic], ch)
-	}
-
-	if len(ps.topics[topic]) == 0 {
-		delete(ps.topics, topic)
-	}
-	ps.mx.Unlock()
-}
-
-func (ps *PubSub) addSub(topic string, once bool) chan any {
-	ch := make(chan any)
-
-	ps.mx.Lock()
+func (ps *PubSub) add(topic string, ch chan any, once bool) {
 	if ps.topics[topic] == nil {
 		ps.topics[topic] = make(map[chan any]bool)
 	}
 
 	ps.topics[topic][ch] = once
-	ps.mx.Unlock()
+}
 
-	return ch
+func (ps *PubSub) send(topic string, msg any) {
+	for ch, once := range ps.topics[topic] {
+		ch <- msg
+		if once {
+			ps.remove(topic, ch)
+		}
+	}
+}
+
+func (ps *PubSub) remove(topic string, ch chan any) {
+	delete(ps.topics[topic], ch)
+	if len(ps.topics[topic]) == 0 {
+		delete(ps.topics, topic)
+	}
 }
