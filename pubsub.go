064fbcf5906bcfeaad4efa66e4d3d9e040184d4f
Chandra Sekar S <chandru.in@gmail.com>
2012-04-01 15:26:24 +0530

    Documentation.

--- pubsub.go
+++ pubsub.go
@@ -13,31 +13,46 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// Package pubsub implements a simple multi-topic pub-sub
+// library.
+//
+// Topic names must be strings and messages of any type
+// can be published. A topic can have any number of subcribers and
+// all of them receive any message published to the topic.
 package pubsub
 
 import "sync"
 
 type any interface{}
 
+// PubSub is a collection of topics.
 type PubSub struct {
 	topics map[string]map[chan any]bool
 	mx     *sync.RWMutex
 }
 
+// New creates a new PubSub.
 func New() *PubSub {
 	topics := make(map[string]map[chan any]bool)
 	mx := new(sync.RWMutex)
 	return &PubSub{topics, mx}
 }
 
+// Sub returns a channel on which messages published to
+// the specified topic can be received.
 func (ps *PubSub) Sub(topic string) chan any {
 	return ps.addSub(topic, false)
 }
 
+// SubOnce is similar to Sub, but only the first message
+// published to the topic can be received.
 func (ps *PubSub) SubOnce(topic string) chan any {
 	return ps.addSub(topic, true)
 }
 
+// Pub publishes the given message to all subscribers of
+// the specified topic.
 func (ps *PubSub) Pub(topic string, msg any) {
 	removes := make([]chan any, 0, 100)
 
@@ -55,6 +70,8 @@ func (ps *PubSub) Pub(topic string, msg any) {
 	}
 }
 
+// Unsub unsubscribed the given channels from the specified
+// topic.
 func (ps *PubSub) Unsub(topic string, chans ...chan any) {
 	ps.mx.Lock()
 	for _, ch := range chans {
