69bd6615d02cf70a319906600ec84dec9326c4c3
Chandra Sekar S <chandru.in@gmail.com>
2018-10-04 07:09:08 +0530

    Move license terms to LICENSE file
    Fixes #15.

--- pubsub.go
+++ pubsub.go
@@ -1,6 +1,6 @@
 // Copyright 2013, Chandra Sekar S.  All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the README.md file.
+// license that can be found in the LICENSE file.
 
 // Package pubsub implements a simple multi-topic pub-sub
 // library.
